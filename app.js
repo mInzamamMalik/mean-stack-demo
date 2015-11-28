dbb57333f4cb7d0b2d3c781943de9985a64a15f1
inzamam malik <malikasinger@gmail.com>
2015-11-28 22:44:56 +0500

    states created

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
 /**
  * Created by malikasinger on 11/28/2015.
  */
+angular.module("myApp",["material","ui.Router"])
+
+.config(function($urlRouterProvider, $stateProvider){
+
+        $urlRouterProvider.otherwise("/");
+
+
+        $stateProvider
+
+            .state("home",{
+                Url : "/",
+                templateUrl : "views/home/home.html",
+                controller : homeController
+            })
+            .state("contact",{
+                Url : "/contact",
+                templateUrl : "views/contact/contact.html",
+                controller : contactController
+            })
+            .state("about",{
+                Url : "/contact",
+                templateUrl : "views/contact/contact.html",
+                controller : contactController
+            })
+            .state("login",{
+                Url : "/login",
+                templateUrl : "views/login/login.html",
+                controller : loginController
+            })
+
+
+    });
