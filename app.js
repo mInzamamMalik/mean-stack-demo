bc659ce168b8d12cfc58f27e0aa901ece282aec9
Inzamam Malik <malikasinger1@gmail.com>
2015-12-17 22:50:02 +0500

    nmjh

--- app.js
+++ app.js
@@ -8,30 +8,35 @@ angular.module("myApp", ['ngMaterial', 'ui.router', 'ngMdIcons'])
         $urlRouterProvider.otherwise("/");
 
 
-        $stateProvider.state("home",{
-                url : "/",
-                templateUrl : "views/home/home.html",
-                controller : "homeController"
+        $stateProvider.state("home", {
+            url: "/",
+            templateUrl: "views/home/home.html",
+            controller: "homeController"
+        })
+            .state("contact", {
+                url: "/contact",
+                templateUrl: "views/contact/contact.html",
+                controller: "contactController"
             })
-            .state("contact",{
-                url : "/contact",
-                templateUrl : "views/contact/contact.html",
-                controller : "contactController"
+            .state("about", {
+                url: "/contact",
+                templateUrl: "views/about/about.html",
+                controller: "aboutController"
             })
-            .state("about",{
-                url : "/contact",
-                templateUrl : "views/about/about.html",
-                controller : "aboutController"
-            })
-            .state("login",{
-                url : "/login",
-                templateUrl : "views/login/login.html",
-                controller : "loginController"
+            .state("login", {
+                url: "/login",
+                templateUrl: "views/login/login.html",
+                controller: "loginController"
             })
             .state("signup", {
                 url: "/signup",
                 templateUrl: "views/signup/signup.html",
                 controller: "signupController"
+            })
+            .state("encryptPassword", {
+                url: "/encryptPassword",
+                templateUrl: "views/encryptPassword/encryptPassword.html",
+                controller: "encryptPasswordController"
             });
 
         // Configure a dark theme with primary foreground yellow
