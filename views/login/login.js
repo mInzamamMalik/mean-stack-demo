96623fc6dcaed28a740d8330a3b8cbd6d7166f49
inzamam malik <malikasinger1@gmail.com>
2015-12-25 21:36:58 +0500

    wegfgarggaag

--- views/login/login.js
+++ views/login/login.js
@@ -2,13 +2,13 @@
  * Created by malikasinger on 11/28/2015.
  */
 angular.module("myApp")
-    .controller("loginController" , function($scope){
+    .controller("loginController" , function($scope,$http){
+
+//start///// toggle "signup and login" form code
 
         $scope.loginFormShow = true;
         $scope.signupFormShow = false;
-
         $scope.toggleSignupLoginFormShow = function () {
-
             if ($scope.loginFormShow) {
                 $scope.loginFormShow = false;
                 $scope.signupFormShow = true;
@@ -16,8 +16,46 @@ angular.module("myApp")
                 $scope.loginFormShow = true;
                 $scope.signupFormShow = false;
             }
+        };
+//end///// toggle "signup and login" form code
 
-        }
+        $scope.doSignup = function(){
 
+            $http({
+                method : "post",
+                url : "http://localhost:3000/teacher-student-app/v1/signup",
+                //url: "http://inzi.herokuapp.com/teacher-student-app/v1/signup",
+                data :  $scope.signupInfo
+
+            }).then(function(err,res){
+
+            });
+
+        };
+
+///encrypt-app/v1/signup
 
     });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
